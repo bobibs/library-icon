60b3ef91f72ea01123f4832884a6cd5f00c56115
bobibasari <bobibasari@gmail.com>
2023-02-19 04:57:16 +0700

    Update

--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as IconArrow } from "./IconArrow";
-export { default as IconBookmark } from "./IconBookmark";
-export { default as IconCircleNotch } from "./IconCircleNotch";
-export { default as IconHeart } from "./IconHeart";
