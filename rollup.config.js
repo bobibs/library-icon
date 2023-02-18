d450d5f1f393fe3bf4b9d47bf1243e4a0343d1c4
bobibasari <bobibasari@gmail.com>
2023-02-19 04:52:05 +0700

    rr-icon-svgr

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import { babel } from '@rollup/plugin-babel';
+import filesize from 'rollup-plugin-filesize';
+import multiEntry from '@rollup/plugin-multi-entry';
+
+const config = {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/index.esm.js',
+    format: 'esm',
+  },
+  external: [/@babel\/runtime/, 'react'],
+  plugins: [
+    babel({
+      babelHelpers: 'runtime',
+      plugins: [
+        '@babel/plugin-transform-runtime'
+      ],
+    }),
+    filesize(),
+    multiEntry(),
+  ],
+};
+
+export default config;
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { default as IconArrow } from "./IconArrow";
+export { default as IconBookmark } from "./IconBookmark";
+export { default as IconCircleNotch } from "./IconCircleNotch";
+export { default as IconHeart } from "./IconHeart";
